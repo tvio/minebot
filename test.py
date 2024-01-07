9f06749338ef15c0dacb50c011c5321948ccb72a
tvio <tvio@centrum.cz>
2024-01-07 10:02:37 +0100

    kolize funguje

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 #central xy?
-# hide curosr
-# colision s jinym sprite
+# predelat na image
+# udelat miny a kolize na miny
+
 # http://programarcadegames.com/index.php?chapter=introduction_to_sprites
 
+import random
 import pygame
 from sys import exit
 
@@ -15,24 +17,41 @@ width = 800
 height = 600 
 
 pygame.init()
+pygame.mouse.set_visible(False)
 screen = pygame.display.set_mode((width,height))
 clock = pygame.time.Clock()
 
 class Player(pygame.sprite.Sprite):
-    
     def __init__(self, color,width,height):
         super().__init__()
         self.image = pygame.Surface([width,height])
         self.image.fill(color)
         self.rect = self.image.get_rect()
-        pygame.draw.ellipse(self.image, color, [0, 0, width, height])
+        pygame.draw.ellipse(self.image, color, [250, 250, width, height])
 
 
+class Prisera(pygame.sprite.Sprite):
+    def __init__(self, color,width,height):
+        super().__init__()
+        self.image = pygame.Surface([width,height])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        pygame.draw.ellipse(self.image, color,[0,0,width,height])   
+        self.rect.x = (random.randint(0,800))
+        self.rect.y = (random.randint(0,600)) 
+    
+    
 
 player = Player(blue,20,20)
-sprites = pygame.sprite.Group()
-sprites.add(player)
+prisera = Prisera(red,15,15)
+#playerList = pygame.sprite.Group()
+#playerList.add(player)
+priseryList = pygame.sprite.Group()
+priseryList.add(prisera)
 
+allSpritesList = pygame.sprite.Group()
+allSpritesList.add(player)
+allSpritesList.add(prisera)
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -44,8 +63,13 @@ while True:
         pos = pygame.mouse.get_pos()
         player.rect.x = pos[0]
         player.rect.y = pos[1]
-        screen.fill(white)
-        sprites.draw(screen)
+        stret = pygame.sprite.spritecollide(player,priseryList,True)
+        if stret:
+            prisera = Prisera(red,15,15)
+            priseryList.add(prisera)
+            allSpritesList.add(prisera)
 
+        screen.fill(white)
+        allSpritesList.draw(screen)
         pygame.display.update()
         clock.tick(60)
